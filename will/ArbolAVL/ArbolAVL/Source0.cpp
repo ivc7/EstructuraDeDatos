7507c17761964b7b3a1066c4393aafcd09ab25c2
Ivan Condori <ivziete@gmail.com>
2019-07-27 12:58:32 -0500

    Graficos

---
+++ will/ArbolAVL/ArbolAVL/Source0.cpp
@@ -0,0 +1,35 @@
+#include "miAVL.h"
+int main()
+{
+	AVL<char> avl;
+	avl.add('m');
+	avl.add('n');
+	avl.add('o');
+	avl.add('l');
+	avl.add('k');
+	avl.add('q');
+  avl.add('p');
+  avl.add('h');
+	avl.add('i');
+	avl.add('a');
+	//avl.add(43);
+	//avl.add(58);
+	//avl.add(75);
+	//avl.add(86);
+	//avl.add(65);
+	//avl.add(70);
+	//avl.add(67);
+	//avl.add(73);
+	//avl.add(93);
+	//avl.add(69);
+	//avl.add(25);
+	//avl.add(66);
+	//avl.add(68);
+	//avl.add(47);
+	//avl.add(62);
+	//avl.add(10);
+	//avl.add(60);
+
+	avl.print();
+	return 0;
+}
